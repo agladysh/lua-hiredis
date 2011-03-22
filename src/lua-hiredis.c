aac372736e3d3718369054a3da818e78c3c3c4c5
Alexander Gladysh <agladysh@gmail.com>
2011-03-22 09:39:27 +0300

    src: implemented command() (except REDIS_REPLY_ARRAY)

--- src/lua-hiredis.c
+++ src/lua-hiredis.c
@@ -20,7 +20,10 @@ extern "C" {
 #define LUAHIREDIS_COPYRIGHT   "Copyright (C) 2011, lua-hiredis authors"
 #define LUAHIREDIS_DESCRIPTION "Bindings for hiredis Redis-client library"
 
-#define LUAHIREDIS_MT "lua-hiredis.connection"
+#define LUAHIREDIS_CONN_MT "lua-hiredis.connection"
+
+#define LUAHIREDIS_NIL_KEY "NIL"
+static const int NIL_TOKEN = 1; /* TODO: Is this the best solution possible? */
 
 typedef struct luahiredis_Enum
 {
@@ -37,19 +40,51 @@ static void reg_enum(lua_State * L, const luahiredis_Enum * e)
   }
 }
 
+/* Error as a reply */
+#define REDIS_ERR_REPLY 255
+
 static const struct luahiredis_Enum Errors[] =
 {
   { "ERR_IO",       REDIS_ERR_IO },
   { "ERR_EOF",      REDIS_ERR_EOF },
   { "ERR_PROTOCOL", REDIS_ERR_PROTOCOL },
   { "ERR_OTHER",    REDIS_ERR_OTHER },
+  { "ERR_REPLY",    REDIS_ERR_REPLY },
 
   { NULL, 0 }
 };
 
+typedef struct luahiredis_Connection
+{
+  redisContext * pContext;
+} luahiredis_Connection;
+
+static redisContext * check_connection(lua_State * L, int idx)
+{
+  luahiredis_Connection * pConn = (luahiredis_Connection *)luaL_checkudata(
+      L, idx, LUAHIREDIS_CONN_MT
+    );
+  if (pConn == NULL)
+  {
+    luaL_error(L, "lua-hiredis error: connection is null");
+    return NULL; /* Unreachable */
+  }
+
+  if (pConn->pContext == NULL)
+  {
+    luaL_error(
+        L, "lua-hiredis error: attempted to use closed connection"
+      );
+    return NULL; /* Unreachable */
+  }
+
+  return pConn->pContext;
+}
+
 /* Call this only if error is already detected */
 static int push_error(lua_State * L, redisContext * pContext)
 {
+  /* TODO: Use errno if err is REDIS_ERR_IO */
   lua_pushnil(L);
   lua_pushstring(L, pContext->errstr);
   lua_pushnumber(L, pContext->err);
@@ -57,36 +92,189 @@ static int push_error(lua_State * L, redisContext * pContext)
   return 3;
 }
 
-typedef struct luahiredis_Connection
+/* TODO: How to get rid of allocations here? */
+
+static void destroy_args(
+    lua_State * L,
+    int nargs,
+    const char *** argv,
+    size_t ** argvlen
+  )
 {
-  redisContext * pContext;
-} luahiredis_Connection;
+  void * alloc_ud = NULL;
+  lua_Alloc alloc_fn = lua_getallocf(L, &alloc_ud);
+
+  alloc_fn(alloc_ud, (void *)*argvlen, nargs * sizeof(size_t), 0UL);
+  *argvlen = NULL;
+
+  alloc_fn(alloc_ud, (void *)*argv, nargs * sizeof(const char *), 0UL);
+  *argv = NULL;
+}
+
+static int create_args(
+    lua_State * L,
+    redisContext * pContext,
+    int idx, /* index of first argument */
+    const char *** argv,
+    size_t ** argvlen
+  )
+{
+  void * alloc_ud = NULL;
+  lua_Alloc alloc_fn = lua_getallocf(L, &alloc_ud);
+
+  int nargs = lua_gettop(L) - idx + 1;
+  int i = 0;
+
+  if (nargs <= 0)
+  {
+    return luaL_error(L, "missing command name");
+  }
+
+  *argv = (const char **)alloc_fn(
+      alloc_ud, NULL, 0UL, nargs * sizeof(const char *)
+    );
+  if (*argv == NULL)
+  {
+    return luaL_error(L, "command: can't allocate argv buffer");
+  }
+
+  *argvlen = (size_t *)alloc_fn(
+      alloc_ud, NULL, 0UL, nargs * sizeof(size_t)
+    );
+  if (*argvlen == NULL)
+  {
+    alloc_fn(alloc_ud, *argv, nargs * sizeof(const char *), 0UL);
+    *argv = NULL;
+
+    return luaL_error(L, "command: can't allocate argvlen buffer");
+  }
+
+  for (i = 0; i < nargs; ++i)
+  {
+    size_t len = 0;
+    const char * str = lua_tolstring(L, idx + i, &len);
+
+    if (str == NULL)
+    {
+      destroy_args(L, nargs, argv, argvlen);
+
+      return luaL_argerror(L, idx + i, "expected a string or number value");
+    }
+
+    (*argv)[i] = str;
+    (*argvlen)[i] = len;
+  }
+
+  return nargs;
+}
 
 static int lconn_command(lua_State * L)
 {
-  return 0; /* TODO */
+  redisContext * pContext = check_connection(L, 1);
+
+  const char ** argv = NULL;
+  size_t * argvlen = NULL;
+  int nargs = create_args(L, pContext, 2, &argv, &argvlen);
+
+  int nret = 0;
+
+  redisReply * pReply = redisCommandArgv(pContext, nargs, argv, argvlen);
+  if (pReply == NULL)
+  {
+    destroy_args(L, nargs, &argv, &argvlen);
+
+    /* TODO: Shouldn't we clear the context error state after this? */
+    return push_error(L, pContext);
+  }
+
+  switch(pReply->type)
+  {
+    case REDIS_REPLY_STATUS:
+      lua_pushlstring(L, pReply->str, pReply->len);
+      nret = 1;
+      break;
+
+    case REDIS_REPLY_ERROR:
+      lua_pushnil(L);
+      lua_pushlstring(L, pReply->str, pReply->len);
+      lua_pushinteger(L, REDIS_ERR_REPLY);
+      nret = 3;
+      break;
+
+    case REDIS_REPLY_INTEGER:
+      lua_pushinteger(L, pReply->integer);
+      nret = 1;
+      break;
+
+    case REDIS_REPLY_NIL:
+      /* TODO: Lazy. Make this overridable? */
+      lua_pushlightuserdata(L, (void *)&NIL_TOKEN);
+      nret = 1;
+      break;
+
+    case REDIS_REPLY_STRING:
+      lua_pushlstring(L, pReply->str, pReply->len);
+      nret = 1;
+      break;
+
+    case REDIS_REPLY_ARRAY:
+      return luaL_error(L, "TODO: Implement");
+      break;
+
+    default: /* should not happen */
+      lua_pushnil(L);
+      lua_pushliteral(L, "command: unknown reply type");
+      nret = 2;
+      break;
+  }
+
+  freeReplyObject(pReply);
+  pReply = NULL;
+
+  destroy_args(L, nargs, &argv, &argvlen);
+
+  return nret;
 }
 
 static int lconn_append_command(lua_State * L)
 {
+  redisContext * pContext = check_connection(L, 1);
+
   return 0; /* TODO */
 }
 
 static int lconn_get_reply(lua_State * L)
 {
+  redisContext * pContext = check_connection(L, 1);
+
   return 0; /* TODO */
 }
 
 static int lconn_close(lua_State * L)
 {
-  return 0; /* TODO */
+  luahiredis_Connection * pConn = (luahiredis_Connection *)luaL_checkudata(
+      L, 1, LUAHIREDIS_CONN_MT
+    );
+
+  if (pConn && pConn->pContext != NULL)
+  {
+    redisFree(pConn->pContext);
+    pConn->pContext = NULL;
+  }
+
+  return 0;
 }
 
 #define lconn_gc lconn_close
 
 static int lconn_tostring(lua_State * L)
 {
-  return 0; /* TODO */
+  redisContext * pContext = check_connection(L, 1);
+
+  /* TODO: Provide more information? */
+  lua_pushliteral(L, "lua-hiredis.connection");
+
+  return 1;
 }
 
 static const luaL_reg M[] =
@@ -110,11 +298,14 @@ static int lhiredis_connect(lua_State * L)
   const char * host = luaL_checkstring(L, 1);
   int port = luaL_checkint(L, 2);
 
+  /* TODO: Support Timeout, Unix and UnixTimeout flavors */
+
   pContext = redisConnect(host, port);
   if (!pContext)
   {
-    /* Should not happen */
-    return luaL_error(L, "failed to create hiredis context");
+    lua_pushnil(L);
+    lua_pushliteral(L, "failed to create hiredis context");
+    return 2;
   }
 
   if (pContext->err)
@@ -130,7 +321,7 @@ static int lhiredis_connect(lua_State * L)
   pResult = lua_newuserdata(L, sizeof(luahiredis_Connection));
   pResult->pContext = pContext;
 
-  if (luaL_newmetatable(L, LUAHIREDIS_MT))
+  if (luaL_newmetatable(L, LUAHIREDIS_CONN_MT))
   {
     luaL_register(L, NULL, M);
     lua_pushvalue(L, -1);
@@ -178,6 +369,12 @@ LUALIB_API int luaopen_hiredis(lua_State * L)
   */
 
   reg_enum(L, Errors);
+
+  /*
+  * Register NIL token
+  */
+  lua_pushlightuserdata(L, (void *)&NIL_TOKEN);
+  lua_setfield(L, -2, LUAHIREDIS_NIL_KEY);
 
   return 1;
 }
--- test/test.lua
+++ test/test.lua
@@ -4,10 +4,16 @@ pcall(require, 'luarocks.require')
 
 local hiredis = require 'hiredis'
 
--- TODO: Remove quick hacks below
-
 print(hiredis.connect("xxx", 1))
 local conn = assert(hiredis.connect("localhost", 6379))
+
+assert(conn:command("SET", "MYKEY", "MYVALUE"))
+assert(assert(conn:command("GET", "MYKEY")) == "MYVALUE")
+
+local NIL = assert(conn:command("GET", "BADKEY"))
+assert(NIL == hiredis.NIL)
+
+conn:close()
 conn:close()
 
-error("TODO: Implement")
+error("TODO: Write better tests")
