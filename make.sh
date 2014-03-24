e15ee4150827431062c228943d10d49143952dac
Alexander Gladysh <agladysh@gmail.com>
2014-03-24 10:06:11 +0400

    make.sh: fixed include paths

--- make.sh
+++ make.sh
@@ -6,14 +6,14 @@ echo "----> Going pedantic all over the source"
 
 # Ugh. hiredis.h is not c89-compatible
 echo "--> c89..."
-gcc -O2 -fPIC -I/usr/include/lua5.1 -c src/lua-hiredis.c -o /dev/null -Isrc/ -Ilib/hiredis/ -Wall --pedantic --std=c89 #-Werror
+gcc -O2 -fPIC -I/usr/include/lua5.1 -c src/lua-hiredis.c -o /dev/null -Isrc/ -Ilib/ -Wall --pedantic --std=c89 #-Werror
 
 echo "--> c99..."
-gcc -O2 -fPIC -I/usr/include/lua5.1 -c src/lua-hiredis.c -o /dev/null -Isrc/ -Ilib/hiredis/ -Wall --pedantic -Werror --std=c99
+gcc -O2 -fPIC -I/usr/include/lua5.1 -c src/lua-hiredis.c -o /dev/null -Isrc/ -Ilib/ -Wall --pedantic -Werror --std=c99
 
 # Ugh. hiredis.h is not c++98-compatible
 echo "--> c++98..."
-gcc -xc++ -O2 -fPIC -I/usr/include/lua5.1 -c src/lua-hiredis.c -o /dev/null -Isrc/c/ -Ilib/hiredis/ --pedantic -Wall --std=c++98 #-Werror
+gcc -xc++ -O2 -fPIC -I/usr/include/lua5.1 -c src/lua-hiredis.c -o /dev/null -Isrc/c/ -Ilib/ --pedantic -Wall --std=c++98 #-Werror
 
 echo "----> Making rock"
 sudo luarocks make rockspec/lua-hiredis-scm-1.rockspec
